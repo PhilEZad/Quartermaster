a55069b0c13869676f1e4636ad31b73bc9c5be09
Philip Zadeh <philezad@gmail.com>
2023-11-28 15:14:19 +0100

    Made Account Service

---
+++ backend/Application/Services/AccountService.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Services;
+
+public class AccountService
+{
+    private readonly IAccountRepository _accountRepository;
+    
+    public AccountService(IAccountRepository accountRepository)
+    {
+        _accountRepository = accountRepository ?? throw new ArgumentNullException("accountRepository is null");
+    }
+    
+}
--- backend/UnitTests/Services/AccountTests.cs
+++ backend/UnitTests/Services/AccountTests.cs
@@ -1,3 +1,4 @@
+using Application.Services;
 using Domain;
 using FluentAssertions;
 
