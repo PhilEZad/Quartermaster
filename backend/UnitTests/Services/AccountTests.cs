43be7114744c2ae688d87b74de536221407030c0
Philip Zadeh <philezad@gmail.com>
2023-11-29 20:42:13 +0100

    Added more CreateAccount unit tests

--- backend/UnitTests/Services/AccountTests.cs
+++ backend/UnitTests/Services/AccountTests.cs
@@ -1,7 +1,9 @@
+using Application.Interfaces;
 using Application.Services;
 using Domain;
 using FluentAssertions;
 using Infrastructure.Repositories;
+using Moq;
 
 namespace UnitTests.Services;
 
@@ -26,5 +28,113 @@ public class AccountTests
     
     /*
      * Account Creation Tests
+     */
+    
+    [Fact]
+    public void CreateAccountWithNull_ShouldThrowNullReferenceException()
+    {
+        var repo = new Mock<IAccountRepository>();
+        AccountService service = new(repo.Object);
+        
+        Action test = () => service.CreateAccount(null);
+
+        test.Should().Throw<NullReferenceException>();
+    }
+    
+    [Fact]
+    public void CreateAccountWithValidObject_ShouldNotThrowNullReferenceException()
+    {
+        var repo = new Mock<IAccountRepository>();
+        AccountService service = new(repo.Object);
+
+        var user = new User
+        {
+            id = 1,
+            userName = "test",
+            password = "test",
+        };
+        
+        Action test = () => service.CreateAccount(user);
+
+        test.Should().NotThrow<NullReferenceException>();
+    }
+    
+    [Fact]
+    public void CreateAccountSuccess_ShouldReturnTrue()
+    {
+        var repo = new Mock<IAccountRepository>();
+        AccountService service = new(repo.Object);
+
+        var user = new User
+        {
+            id = 1,
+            userName = "test",
+            password = "test",
+        };
+        
+        var result = service.CreateAccount(user);
+
+        result.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void CreateAccountFailure_ShouldReturnFalse()
+    {
+        var repo = new Mock<IAccountRepository>();
+        AccountService service = new(repo.Object);
+
+        var user = new User
+        {
+            id = 1,
+            userName = "test",
+            password = "test",
+        };
+        
+        var result = service.CreateAccount(user);
+
+        result.Should().BeFalse();
+    }
+    
+    [Fact]
+    public void CreateAccountWithExistingUser_ShouldReturnMessage()
+    {
+        var repo = new Mock<IAccountRepository>();
+        AccountService service = new(repo.Object);
+
+        var user = new User
+        {
+            id = 1,
+            userName = "test",
+            password = "test",
+        };
+        
+        var result = service.CreateAccount(user);
+
+        result.Should().Be("User already exists");
+    }
+    
+    [Theory]
+    [InlineData(null,"Password can not be null")]
+    [InlineData("", "Password can not be empty")]
+    [InlineData("1234","Password must be more than 8 characters")]
+    public void CreateAccountWithInvalidPassword_ShouldReturnMessage(String password, String errorMessage)
+    {
+        var repo = new Mock<IAccountRepository>();
+        AccountService service = new(repo.Object);
+
+        var user = new User
+        {
+            id = 1,
+            userName = "test",
+            password = password,
+        };
+        
+        var result = service.CreateAccount(user);
+
+        result.Should().Be(errorMessage);
+    }
+    
+    /*
+     * Account Login Tests
      */
 }
