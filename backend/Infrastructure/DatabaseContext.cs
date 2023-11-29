c0a30587372667f189a213aa8fdd27656f9d8705
Philip Zadeh <philezad@gmail.com>
2023-11-29 16:32:47 +0100

    Added msSQL database and config for EntityFramework

---
+++ backend/Infrastructure/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+public class DatabaseContext : DbContext
+{
+    private readonly IConfiguration _config;
+    
+    public DatabaseContext(IConfiguration config)
+    {
+        _config = config ?? throw new NullReferenceException();
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_config.GetConnectionString("DatabaseConnection"));
+    }
+    
+    public DbSet<User> Users { get; set; }
+}
