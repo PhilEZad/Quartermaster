c8cdf28b6831a40ffcc2dc6872e6b4d117364cf0
Philip Zadeh <philezad@gmail.com>
2023-12-05 21:31:07 +0100

    Added validators for weapon, weapon request/reponse

---
+++ backend/Application/Validators/WeaponValidators.cs
@@ -0,0 +1,48 @@
+﻿using Application.DTOs;
+using Application.DTOs.Responses;
+using Domain;
+using FluentValidation;
+
+namespace Application.Validators;
+
+public class WeaponValidator : AbstractValidator<Weapon>
+{
+    public WeaponValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Range).NotEmpty();
+        RuleFor(x => x.Type).NotEmpty();
+        RuleFor(x => x.Strength).NotEmpty();
+        RuleFor(x => x.ArmourPenetration).NotEmpty();
+        RuleFor(x => x.Damage).NotEmpty();
+        RuleFor(x => x.Points).NotEmpty();
+    }
+}
+
+public class WeaponRequestValidator : AbstractValidator<WeaponRequest>
+{
+    public WeaponRequestValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Range).NotEmpty();
+        RuleFor(x => x.Type).NotEmpty();
+        RuleFor(x => x.Strength).NotEmpty();
+        RuleFor(x => x.ArmourPenetration).NotEmpty();
+        RuleFor(x => x.Damage).NotEmpty();
+        RuleFor(x => x.Points).NotEmpty();
+    }
+}
+
+public class WeaponResponseValidator : AbstractValidator<WeaponResponse>
+{
+    public WeaponResponseValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Range).NotEmpty();
+        RuleFor(x => x.Type).NotEmpty();
+        RuleFor(x => x.Strength).NotEmpty();
+        RuleFor(x => x.ArmourPenetration).NotEmpty();
+        RuleFor(x => x.Damage).NotEmpty();
+        RuleFor(x => x.Points).NotEmpty();
+    }
+}
