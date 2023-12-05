f110c3be33c7322e7bd0d146cb8a219c0b9c71b4
Philip Zadeh <philezad@gmail.com>
2023-12-05 21:24:07 +0100

    Added properties to Weapon Request/Response

--- backend/Application/DTOs/Requests/WeaponRequest.cs
+++ backend/Application/DTOs/Requests/WeaponRequest.cs
@@ -1,6 +1,18 @@
-﻿namespace Application.DTOs;
+﻿using Domain;
+
+namespace Application.DTOs;
 
 public class WeaponRequest
 {
+    public string Name { get; set; }
     
+    public int Range { get; set; }
+    public string Type { get; set; }
+    public int Strength { get; set; }
+    public int ArmourPenetration { get; set; }
+    public int Damage { get; set; }
+    
+    public List<Ability> Abilities { get; set; }
+
+    public int Points { get; set; }
 }
--- backend/Application/DTOs/Responses/WeaponResponse.cs
+++ backend/Application/DTOs/Responses/WeaponResponse.cs
@@ -1,6 +1,19 @@
-﻿namespace Application.DTOs.Responses;
+﻿using Domain;
+
+namespace Application.DTOs.Responses;
 
 public class WeaponResponse
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
     
+    public int Range { get; set; }
+    public string Type { get; set; }
+    public int Strength { get; set; }
+    public int ArmourPenetration { get; set; }
+    public int Damage { get; set; }
+    
+    public List<Ability> Abilities { get; set; }
+
+    public int Points { get; set; }
 }
