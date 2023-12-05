49818f3215a0c9f6a026c3611015b10a9559135f
Philip Zadeh <philezad@gmail.com>
2023-12-05 16:57:59 +0100

    Setup Unit validators

---
+++ backend/Application/Validators/UnitValdiators.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using Application.DTOs;
+using Domain;
+using FluentValidation;
+
+namespace Application.Validators;
+
+public class UnitValdiator : AbstractValidator<Unit>
+{
+    public UnitValdiator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+    }
+}
+
+public class UnitRequestValidator : AbstractValidator<UnitRequest>
+{
+    public UnitRequestValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+    }
+}
